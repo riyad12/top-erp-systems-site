702d2aa409091f927cc03e44fff0b7f437eb5ce3
riyad12 <mangekujang2@gmail.com>
2019-07-09 02:13:20 +0700

    Update erp-utama.js

--- erp-utama.js
+++ erp-utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2019/07/3-process-improvement-lessons-from-financial-firms.html
 if(x=="2"){xxx="/2019/07/artificial-intelligence-improves-erp-software.html"}
 if(x=="3"){xxx="/2019/07/5-tips-for-prioritizing-erp-equirements.html"}
 if(x=="4"){xxx="/2019/07/cost-to-implement-an-erp-system.html"}
-if(x=="5"){xxx="/2019/06/erp-data-migration-and-cleansing-tips.html"}a_to_ve[i].href="http://www.onicnetwork.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2019/06/erp-data-migration-and-cleansing-tips.html"}a_to_ve[i].href="https://www.top-erp-systems.site"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
